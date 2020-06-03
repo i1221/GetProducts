001977c4e626b30fb086150939db0e633105e005
i1221 <i122123@yandex.by>
2020-06-03 21:25:28 +0300

    Com_1
    Com_1_Descript

---
+++ WebApplCore/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+// using WebApplCore.Core.Services.Interfaces;
+using WebApplCore.Models;
+
+namespace WebApplCore.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> productList;
+        
+        public ProductController()
+        {
+            productList = new List<Product>
+            {
+                new Product {Id = 1, Name = "Name_1", Description = "Description_1", Price = 21, UnitsInStock = 31},
+                new Product {Id = 2, Name = "Name_2", Description = "Description_2", Price = 22, UnitsInStock = 31},
+                new Product {Id = 3, Name = "Name_3", Description = "Description_3", Price = 23, UnitsInStock = 33}
+            };
+        }
+
+        public ActionResult Index()
+        {
+            // var model = articlesRepository.GetArticles();
+            var model = productList;
+            
+            return View(model);
+        }
+
+        public PartialViewResult GetProducts(string Name)
+        {
+            // var model = _productService.companySelectList;
+            // ViewBag.Companies = _productService.companySelectList;
+             var model = productList;
+
+            return PartialView(model);
+        }
+
+    }
+}
+
---
+++ WebApplCore/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//
+// using Microsoft.AspNetCore.Mvc.Rendering;
+// using System.Collections.Generic;
+
+namespace WebApplCore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int UnitsInStock { get; set; }
+
+    }
+}
